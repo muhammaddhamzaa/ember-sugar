307e1296a7547643313826e303c5a07435c982d9
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-10-24 16:31:37 +0500

    "First Commit"

---
+++ app/controllers/login-page.js
@@ -0,0 +1,28 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+
+export default Controller.extend({
+  ajax: Ember.inject.service(),
+  actions: {
+    submit: function(){
+      this.get('ajax').request('http://127.0.0.1/SugarPro-Full-8.0.0/rest/v10/oauth2/token',{
+        method: 'POST',
+        headers:{
+        'Content-Type': 'application/json'
+        },
+        data:{
+          "grant_type": "password",
+          "client_id": "sugar",
+          "client_secret": "",
+          "username": this.get('user'),
+          "password": this.get('pass'),
+          "platform": "base"
+        },
+        success: function(data){
+          console.log(data);
+        },
+        dataType: 'json'
+      });
+    }
+  }
+});
