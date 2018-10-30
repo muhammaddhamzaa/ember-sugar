696c6446fd0c72b3186b6b5ea7d9b2d7d031b366
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-10-30 10:34:31 +0500

    Fourth revision - display issue fixed

--- app/serializers/lead.js
+++ app/serializers/lead.js
@@ -5,8 +5,7 @@ import Ember from 'ember';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
   normalizeResponse(store, primaryModelClass, payload, id, requestType){
-    payload = {
-      'leads': payload.records};
+    payload = {'leads': payload.records};
     console.log(payload);
     return this._super(store, primaryModelClass, payload, id, requestType);
   }
